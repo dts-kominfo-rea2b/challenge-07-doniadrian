ddeec58fdc0ae6a24e3a4ca2b4d0410c3d48940d
Doni Adrian <doniadrian9@gmail.com>
2022-06-12 21:25:18 +0700

    update challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (expression) => {
+  try {
+    const [ixx, vgc] = await Promise.all([
+      promiseTheaterIXX(),
+      promiseTheaterVGC(),
+    ]);
+    const result = [...ixx, ...vgc];
+    return result.filter((item) => item.hasil === expression).length;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
